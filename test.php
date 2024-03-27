a4de35b8749d947c2a018930620ba43afb76a7d5
Ait-Meddour Fouad Eddine <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2024-03-27 22:01:09 +0100

    test.php

--- test.php
+++ test.php
@@ -1,15 +1,50 @@
 
 <?php
+
+session_start();
+
+
 $servername = "srv1160.hstgr.io";
 $username = "u209047910_echoes";
 $password = "Echoes123#";
+$dbname = "u209047910_echoes";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
 
-// Check connection
+$id = $_POST['id'];
+$password = $_POST['password'];
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connexion échouée : " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+
+$sql = "SELECT * FROM Team WHERE id = '$id' AND password = '$password'";
+$result = $conn->query($sql);
+
+
+if ($result->num_rows > 0) {
+
+    $row = $result->fetch_assoc();
+    $_SESSION['team_id'] = $row["id"]; // Stocker l'ID de l'équipe dans la session
+
+    // Mettre à jour la dernière connexion de l'équipe
+    $teamId = $row["id"];
+    $date = date("Y-m-d");
+    $updateSql = "UPDATE Team SET lastLogin = '$date' WHERE id = '$teamId'";
+    if ($conn->query($updateSql) === TRUE) {
+        echo "Connexion réussie ! Dernière connexion mise à jour.";
+    } else {
+        echo "Erreur lors de la mise à jour de la dernière connexion : " . $conn->error;
+    }
+} else {
+
+    echo "ID ou mot de passe incorrect.";
+}
+
+
+$conn->close();
 ?>
