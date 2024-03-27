60486a24f3090746f058853e9ec8d5903c0b7ed9
Ait-Meddour Fouad Eddine <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2024-03-27 14:36:55 +0100

    test.php

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 
 <?php
+$servername = "srv1160.hstgr.io";
+$username = "u209047910_echoes";
+$password = "Echoes123#";
 
-// Hello World program
-echo "Hello World!";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
