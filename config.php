562e8711210fd100b32d7e539eec256f40043484
gayamedj <medjdoubgaya@gmail.com>
2024-04-03 20:56:22 +0100

    chapter1

--- config.php
+++ config.php
@@ -8,4 +8,13 @@
         'DATABASE' => 'u209047910_echoes'
     ];
 
+    // // For dev
+    // $ENV = [
+    //     'ENVIRONMENT' => 'development',
+    //     'HOST' => 'srv1160.hstgr.io',
+    //     'USERNAME' => 'u209047910_echoes_test',
+    //     'PASSWORD' => 'Echoes123#',
+    //     'DATABASE' => 'u209047910_echoes_test'
+    // ];
+
 ?>
